f0509fb49d2c0a78aaefbf777e39b722ba25f734
Vertexwahn <julian.amann@tum.de>
2023-03-18 00:05:02 +0100

    Update release_prep.sh
    GitOrigin-RevId: 2cee8605d47aa797ccb84af6b53e090cbf79620f

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -11,8 +11,8 @@ set -o errexit -o nounset -o pipefail
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
 # The prefix is chosen to match what GitHub generates for source archives
-PREFIX="rules_mylang-${TAG:1}"
-ARCHIVE="rules_mylang-$TAG.tar.gz"
+PREFIX="rules_ispc-${TAG:1}"
+ARCHIVE="rules_ispc-$TAG.tar.gz"
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
