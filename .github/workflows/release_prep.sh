bdff08e2b6ce61461b3e6cefe20aa18a8432097b
Vertexwahn <julian.amann@tum.de>
2025-08-15 02:39:25 +0200

    Fix issue with creating release archive
    GitOrigin-RevId: 5567cbdaeb78053d2c0acf31ccf458d729b581e9

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -11,7 +11,7 @@ set -o errexit -o nounset -o pipefail
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
 # The prefix is chosen to match what GitHub generates for source archives
-PREFIX="rules_ispc-${TAG:1}"
+PREFIX="rules_ispc-${TAG}"
 ARCHIVE="rules_ispc-$TAG.tar.gz"
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
